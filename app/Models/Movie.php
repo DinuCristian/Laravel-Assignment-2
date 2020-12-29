8ad5f506afcfcea86360975c603d78de6259ff49
DinuCristian <bringrednight@yahoo.com>
2020-12-29 18:40:44 +0200

    Email movie details - issue #3

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -29,4 +29,19 @@ class Movie extends Model
     {
         return Helpers::fakeImageUrl($this->id, $width, $height);
     }
+
+    public function formattedNotes()
+    {
+        return Helpers::paragraphsAsHtml($this->description, 4);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable')->whereNull('parent_id');
+    }
 }
--- resources/views/movie/show.blade.php
+++ resources/views/movie/show.blade.php
@@ -35,6 +35,20 @@
                         </div>
                         <p class="block mt-1 text-lg leading-tight font-semibold text-gray-900">{{ $movie->genre }}</p>
                         <p class="mt-2 text-gray-600">{{ $movie->description }}</p>
+
+                        <div class="ml-4">
+                            <div class="mt-2 flex items-center text.black">
+                                <a href="{{ $movie->path('email') }}"
+                                   style="display: inline-block; white-space: nowrap; vertical-align: top">
+                                    <svg xmlns="http://www.w3.org/2000/svg" style="white-space: nowrap; display: inline-block"
+                                         viewBox="0 0 20 20" fill="currentColor" width="25" height="25">
+                                        <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"/>
+                                        <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"/>
+                                    </svg>
+                                    <span>{{ __("Email Details") }}</span>
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
